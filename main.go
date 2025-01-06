718e3f734a02f96a61d03388618641a3675a6428
AlwaysNur <info@nursplace.dev>
2025-01-06 18:18:39 +1300

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	api "feed/api"
+
 	staticserver "feed/static_server"
 	"fmt"
+
 	"log"
+
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
 	r := mux.NewRouter()
 	fmt.Println("Starting server on http://localhost:8080")
 	fmt.Println("Starting API on http://localhost:8080/api")
